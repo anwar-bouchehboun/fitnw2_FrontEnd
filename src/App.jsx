cda70ef6b52676c2c4decc236c48ee68e013acbc
anouar <anouar.ab95@gmail.com>
2024-04-01 14:57:24 +0000

    create dossier

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import SingIn from './Auth/component/Login'
+import Singup from './Auth/component/Register'
+
+function App() {
+
+  return (
+    <>
+    <Singup />
+    </>
+  )
+}
+
+export default App
